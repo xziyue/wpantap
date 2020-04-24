c52b8a3ab8db8d4abf041909ca05add7dd4c6326
Alan Xiang <xziyue@syr.edu>
2020-04-23 20:15:43 -0400

    update

---
+++ test/test_read.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arpa/inet.h> 
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <stdarg.h>
+
+int main(){
+
+	int fd = open("/dev/net/wpantap", O_RDWR);
+	if (fd < 0){
+		perror("open");
+		printf("unable to open wpantap device\n");
+		return 1;
+	}
+	
+	char buf[300];
+	int bytes = read(fd, buf, 300);
+	printf("read %d bytes\n", bytes);
+
+	close(fd);
+}
