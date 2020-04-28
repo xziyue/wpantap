fd67f1dcfd91850a42ea25714f32aa49905774b6
Alan Xiang <xziyue@syr.edu>
2020-04-28 16:52:22 -0400

    update lowpan_setup

--- test/lowpan_setup.sh
+++ test/lowpan_setup.sh
@@ -2,12 +2,19 @@
 
 panid="0xbeef"
 
+iwpandev=`iwpan dev`
 
-PHYNUM=`iwpan dev | grep -B 1 wpan0 | sed -ne '1 s/phy#\([0-9]\)/\1/p'`
+echo "WPAN Interface Info:"
+echo "$iwpandev"
+
+PHYNUM=`echo "$iwpandev" | grep -B 1 wpan0 | sed -ne '1 s/phy#\([0-9]\)/\1/p'`
+
+echo "WPAN PHY=${PHYNUM}"
 
 ip netns delete wpan0
 ip netns add wpan0
 iwpan phy${PHYNUM} set netns name wpan0
+
 
 ip netns exec wpan0 iwpan dev wpan0 set pan_id $panid
 ip netns exec wpan0 ip link add link wpan0 name lowpan0 type lowpan
